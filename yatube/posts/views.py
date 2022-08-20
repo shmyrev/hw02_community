b3dc556d00d9b7921502a48925077bf75cb52a98
Дмитрий Шмырев <s1mdm@yandex.ru>
2022-08-20 16:47:43 +0300

    рабочая версия 1

---
+++ yatube/posts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+
+def index(request):
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    template = 'posts/group_list.html'
+    title = 'Здесь будет информация о группах проекта Yatube'
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
+
